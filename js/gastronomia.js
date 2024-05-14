66268911bdc1b45b673e716ef793365e908c194f
HappyNiceDev <04nicolas.k@gmail.com>
2024-05-14 07:02:26 -0300

    Nuevos efectos y animaciones del totem

--- js/gastronomia.js
+++ js/gastronomia.js
@@ -22,11 +22,17 @@ const contenedorContenido = document.querySelector('.Contenido');
 // Obtener todos los elementos de contenido
 const elementosContenido = contenedorContenido.querySelectorAll('div');
 
+const elementoTotem = document.getElementById('Totem');
+
+
+
 
 // Agregar un evento click a cada imagen del totem
-document.querySelectorAll('.Totem > div').forEach((imagen, index) => {
+document.querySelectorAll('#Totem > div').forEach((imagen, index) => {
 
   imagen.addEventListener('click', function (event) {
+
+    CambioClases();
 
     handleClickImagen(event, index);
 
@@ -48,10 +54,28 @@ function handleClickImagen(event, index) {
 
   // Mostrar el elemento de contenido correspondiente al índice de la imagen clicada
   elementosContenido[index].style.display = 'block';
+
 }
 
 
 
+//Se detecta click sobre alguna imagen del totem y se desencadena el cambio de las clases.
+function CambioClases() {
+
+  elementoTotem.classList.replace('TotemCentrado', 'Totem_lateral');
+  elementoTotem.classList.replace('TotemCentrado', 'Totem_lateral');
+  elementoTotem.style.animation = 'TotemCentradoSalida 1s ease-in-out forwards;';
+
+  document.querySelectorAll('#Totem > div').forEach((imagen, index) => {
+
+    let NomClass = 'Img' + (index + 1) + '_lateral';
+    imagen.classList.add(NomClass);
+    
+  });
+}
+
+
+//Detecta y aplica todo los clicks aplicados a los corazones
 const heartIcon = document.querySelectorAll(".like-button .heart-icon");
 
 heartIcon.forEach(x => {
