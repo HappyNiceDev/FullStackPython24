759d3e2536c4d99cd8f94088c315a754462df90b
Natalia <natalia_m811@hotmail.com>
2024-05-18 15:32:53 -0300

    Slideshow js
    Se agrega automatización del slide con función de cambio automático cada 4 segundos, función de detener la automatización mientras el mouse se encuentre arriba del slide y reiniciar la automatización al quitar el mouse del slide.

--- js/main.js
+++ js/main.js
@@ -19,6 +19,53 @@ $("#nav-toggle").click(function () {
 //AQUÍ TERMINA EL JS DEL NAV BAR
 
 
+//AQUI EMPIEZA JS DEL SLIDE
+document.addEventListener("DOMContentLoaded", function() {
+  var slides = document.querySelectorAll('.slider .slide');
+  var radioButtons = document.querySelectorAll('input[name="grupo"]');
+  var currentSlide = 0;
+
+  // Función para mostrar el slide actual
+  function showSlide(index) {
+    slides.forEach(function(slide) {
+      slide.classList.remove('active');
+    });
+    slides[index].classList.add('active');
+  }
+
+  // Función para cambiar al siguiente slide
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+    // Selecciona el botón de radio correspondiente
+    radioButtons[currentSlide].checked = true;
+  }
+
+  // Cambia al siguiente slide cada 4 segundos
+  var slideInterval = setInterval(nextSlide, 4000);
+
+  // Detiene el intervalo cuando el mouse está sobre el slider
+  document.querySelector('.slider').addEventListener('mouseenter', function() {
+    clearInterval(slideInterval);
+  });
+
+  // Reinicia el intervalo cuando el mouse sale del slider
+  document.querySelector('.slider').addEventListener('mouseleave', function() {
+    slideInterval = setInterval(nextSlide, 4000);
+  });
+
+  // Cambia al slide seleccionado por el usuario
+  radioButtons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+      currentSlide = index;
+      showSlide(currentSlide);
+    });
+  });
+});
+
+//AQUÍ TERMINA EL JS DEL SLIDE
+
+
 document.addEventListener("DOMContentLoaded", function () { //Espera que cargue todo el html
 const dd = document.documentElement;
 
