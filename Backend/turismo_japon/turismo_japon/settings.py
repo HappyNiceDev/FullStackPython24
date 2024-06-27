21e086f6295672d1c7d873ed0b70d3448086e568
Natalia <natalia_m811@hotmail.com>
2024-06-27 01:50:30 -0300

    Static
    Se modifican configuraciones para que se vean las imagenes. Se pasan todos los archivos de static a una unica carpeta a la altura del proyecto

--- Backend/turismo_japon/turismo_japon/settings.py
+++ Backend/turismo_japon/turismo_japon/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [ BASE_DIR / 'static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
