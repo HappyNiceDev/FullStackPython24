d97a853b7c2ae9d2811ad92ed50c532a090145c2
HappyNiceDev <04nicolas.k@gmail.com>
2024-06-28 23:29:45 -0300

    Entorno virtual_nico

--- Backend/turismo_japon/turismo_japon/settings.py
+++ Backend/turismo_japon/turismo_japon/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'turismo_japon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        	'ENGINE': 'django.db.backends.postgresql',
+       		'NAME': 'JapanDB_prueba',
+        	'USER': 'root',
+        	'PASSWORD': 'aun no lo se!',
+       		'HOST': '10.102.0.2', # o la dirección IP de tu servidor de base de datos
+       		'PORT': '5432',       # el puerto predeterminado de PostgreSQL
+
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
