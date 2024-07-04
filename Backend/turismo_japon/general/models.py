5d57a76756ad33deac31228a448781cd4744f827
Natalia <natalia_m811@hotmail.com>
2024-07-04 01:12:13 -0300

    Pruebo generar migraciones nuevamente
    Se comenta clase ruta_destino en model.py porque genera errores

--- Backend/turismo_japon/general/models.py
+++ Backend/turismo_japon/general/models.py
@@ -8,83 +8,27 @@
 from django.db import models
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
 class BlogSaved(models.Model):
-    coment_id = models.TextField(primary_key=True)  # This field type is a guess.
-    user_id = models.IntegerField(blank=True, null=True)
-    destin_id = models.IntegerField(blank=True, null=True)
+    comentario_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('Persona', models.DO_NOTHING)
+    destinos = models.ForeignKey('Destinos', models.DO_NOTHING)
 
     class Meta:
         managed = False
         db_table = 'blog_saved'
+        db_table_comment = 'Tabla que contendra los blog guardados por el usuario'
+
+
+class Comentarios(models.Model):
+    comentario = models.ForeignKey(BlogSaved, models.DO_NOTHING)
+    user = models.ForeignKey('Persona', models.DO_NOTHING)
+    tema = models.TextField(blank=True, null=True)
+    img_tema = models.BinaryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'comentarios'
+        db_table_comment = 'Tabla de comentarios sobre las tematicas del usuario'
 
 
 class Contacto(models.Model):
@@ -103,104 +47,90 @@ class Contacto(models.Model):
         db_table = 'contacto'
 
 
-class DataUser(models.Model):
-    user_id = models.IntegerField(primary_key=True)
-    usuario = models.TextField(blank=True, null=True)  # This field type is a guess.
-    e_mail = models.TextField(db_column='e-mail', blank=True, null=True)  # Field renamed to remove unsuitable characters. This field type is a guess.
-    telefono = models.IntegerField(blank=True, null=True)
-    genero = models.TextField(blank=True, null=True)  # This field type is a guess.
-    fech_nac = models.IntegerField(blank=True, null=True)
-    nacion_user = models.TextField(blank=True, null=True)  # This field type is a guess.
-    img_user = models.BinaryField(blank=True, null=True)
+class Destinos(models.Model):
+    destino_id = models.AutoField(primary_key=True)
+    nombre_destino = models.TextField(blank=True, null=True)
+    direccion_completa = models.TextField(blank=True, null=True)
+    longitud = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    latitud = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    tematica = models.TextField(blank=True, null=True)
+    img_tematica = models.BinaryField(blank=True, null=True)
+    peso = models.BinaryField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'data_user'
+        db_table = 'destinos'
+        db_table_comment = 'Tabla que me muestra los destinos elegidos'
 
 
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
+class Persona(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    usuario = models.TextField(blank=True, null=True)
+    mail = models.TextField(blank=True, null=True)
+    telefono = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    genero = models.TextField(blank=True, null=True)
+    fech_nac = models.DateField(blank=True, null=True)
+    pais = models.TextField(blank=True, null=True)
+    avatar = models.BinaryField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
-class ImgTematic(models.Model):
-    user_id = models.IntegerField()
-    tematica = models.TextField(blank=True, null=True)  # This field type is a guess.
-    img_tematica = models.TextField(blank=True, null=True)  # This field type is a guess.
-
-    class Meta:
-        managed = False
-        db_table = 'img_tematic'
+        db_table = 'persona'
+        db_table_comment = 'Tabla que describe los datos de la persona'
 
 
 class RoutesSaved(models.Model):
-    route_id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField(blank=True, null=True)
-    nomb_route = models.TextField(blank=True, null=True)  # This field type is a guess.
-    date_create = models.IntegerField(blank=True, null=True)
+    ruta_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Persona, models.DO_NOTHING)
+    nom_ruta = models.TextField(blank=True, null=True)
+    date_creac = models.TextField(blank=True, null=True)  # This field type is a guess.
     position_dom = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'routes_saved'
+        db_table_comment = 'Tabla que me indica las rutas guardadas por el usuario'
+
+#Se comenta porque esta generando errores
+# class RutasDestino(models.Model):
+#    ruta_id = models.AutoField()
+#    destino_id = models.AutoField()
+#
+#    class Meta:
+#        managed = False
+#        db_table = 'rutas_destino'
+#        db_table_comment = 'Tabla que me muestra las rutas destino identificadas'
 
 
-class SecureUser(models.Model):
-    user_id = models.IntegerField()
-    user_pass = models.TextField(blank=True, null=True)  # This field type is a guess.
+class Seguridad(models.Model):
+    user = models.ForeignKey(Persona, models.DO_NOTHING)
+    psw = models.CharField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'secure_user'
+        db_table = 'seguridad'
+        db_table_comment = 'Tabla que me indica id de usuario y contrase±a'
 
 
-class UserSesion(models.Model):
-    user_id = models.IntegerField()
-    token_user = models.TextField(blank=True, null=True)  # This field type is a guess.
+class Sesion(models.Model):
+    user = models.ForeignKey(Persona, models.DO_NOTHING)
+    token = models.IntegerField(blank=True, null=True)
     pc_mac = models.TextField(blank=True, null=True)  # This field type is a guess.
-    ip_public = models.IntegerField(blank=True, null=True)
-    hour_sesion = models.TimeField(blank=True, null=True)
+    ip_public = models.GenericIPAddressField(blank=True, null=True)
+    time_sesion = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'user_sesion'
+        db_table = 'sesion'
+        db_table_comment = 'Datos de la sesion'
+
+
+class Tematic(models.Model):
+    user = models.ForeignKey(Persona, models.DO_NOTHING)
+    tema = models.TextField(blank=True, null=True)
+    img_tema = models.BinaryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tematic'
+        db_table_comment = 'Tematica del usuario'
