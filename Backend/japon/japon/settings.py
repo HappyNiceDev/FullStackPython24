ade7f3cfae98b5d0436e9e1b0d388bd6d4fb3ddd
HappyNiceDev <04nicolas.k@gmail.com>
2024-07-19 06:33:36 -0300

    se desactiva el debug-log

--- Backend/japon/japon/settings.py
+++ Backend/japon/japon/settings.py
@@ -143,6 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+'''
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -161,4 +162,4 @@ LOGGING = {
         },
     },
 }
-
+'''
